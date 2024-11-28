10f6225881120a5ccadd98d35ba94470cff8790d
MFH1118 <mishalalhaidar@gmail.com>
2024-11-28 18:12:44 +1100

    further configuration of package.json, dockerfile, and index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.BACKEND_PORT
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 const run = async () => {
-  app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 };
 
