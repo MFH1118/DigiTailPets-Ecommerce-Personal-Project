4c93626678993c4e003519937afb863efcd452a0
MFH1118 <mishalalhaidar@gmail.com>
2024-12-14 19:08:09 +1100

    define user types for prisma user model

--- backend/src/types/user.types.ts
+++ backend/src/types/user.types.ts
@@ -3,16 +3,53 @@
 */
 
 // User Registration data
-export interface UserRegistration {
+export interface User {
+    userId: string;
+    userName: string;
     email: string;
-    password: string;
     firstName: string;
     lastName: string;
+    dob: Date;
+    dateCreated: Date;
+    lastLogin: Date | null;
+    isActive: boolean;
 }
 
-export interface UserRole extends UserRegistration {
-    id: string;
-    role: string;
-
+// User Authentication data
+export interface Authentication {
+    authId: string;
+    userId: string;
+    authPasswordHash: string;
+    authPassowordReset: boolean;
+    authLoginAttempts: number;
+    authLockoutEndTime: Date | null;
 }
 
+// User Role data
+export interface Role {
+    roleId: string;
+    roleName: string;
+    roleDescription: string;
+}
+
+// User Registration Client Request
+export interface UserRegistrationRequest {
+    userName: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    dob: string;
+    password: string;
+}
+
+// User Registration Server Response
+export interface UserRegistrationResponse {
+    user: Omit<User, 'last_login'>
+    message: string;
+}
+
+// Error Response Type
+export interface ErrorResponse {
+    error: string;
+    details?: string;
+}
