e1322f7413242d64bdc7d8d5cea01ae015894495
MFH1118 <mishalalhaidar@gmail.com>
2024-12-21 22:21:57 +1100

    fixed index.ts to add the route and check if the api is working correctly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
-import express, { Express, Request, Response} from 'express';
+// src/index.ts
+
+import express, { Express, Request, Response, Application} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js'
 import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
@@ -10,11 +14,21 @@ const prisma: PrismaClient = new PrismaClient();
 const PORT: number = parseInt(process.env.BACKEND_PORT || '5000', 10);
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/auth/', authRoutes);
 
-
+// Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Worlds!');
+});
+
+
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'ok'
+  });
 });
 
 const run = async (): Promise<void> => {
