746f2eb9de6d2ed55fd26043d7c25722cac59a71
MFH1118 <mishalalhaidar@gmail.com>
2024-12-23 00:51:16 +1100

    added the new middleware into auth.routes.ts

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 // src/routes/auth.routes.ts
-
 import express, { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
+import { validateRegistrationInput } from '../middleware/validate.middleware.js';
 
 const router: Router = express.Router();
 
 // Registration endpoint
-router.post('/register', AuthController.registerUser);
+router.post('/register', validateRegistrationInput, AuthController.registerUser);
 
 
 export default router;
