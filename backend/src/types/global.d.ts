746497dccc44b9016dd9a6f42d43f689587bd3c1
MFH1118 <mishalalhaidar@gmail.com>
2025-01-04 04:42:43 +1100

    create a global type for userId for request

---
+++ backend/src/types/global.d.ts
@@ -0,0 +1,12 @@
+// src/types/global.d.ts
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                userId: string;
+            };
+        }
+    }
+}
+
+export {};
