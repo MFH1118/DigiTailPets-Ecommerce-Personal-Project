11fda8bdab52aff4294ce891bee5da7e22055c79
MFH1118 <mishalalhaidar@gmail.com>
2025-01-09 10:03:01 +1100

    added product and category routes and modified index.ts to use the routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import express, { Express, Request, Response, Application} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js'
+import productRoutes from './routes/product.routes.js';
+import categoryRoutes from './routes/category.routes.js';
 import prisma from './db/prisma.js';
 import { userCache } from './model/user.model.js';
 import { getSessionTypeInfo } from './utils/session-type.utils.js';
@@ -21,8 +23,10 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/auth/', authRoutes);
-app.use('/api/addresses/', addressRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/addresses', addressRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
 app.use(useragent.express());
 
 // Routes
