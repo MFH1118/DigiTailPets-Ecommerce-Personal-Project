31e7285cfa5b8167e4e7bce9a89f4ad6069da79a
MFH1118 <mishalalhaidar@gmail.com>
2025-01-16 21:05:39 +1100

    added admin registration routes

---
+++ backend/src/admin/routes/auth.routes.ts
@@ -0,0 +1,16 @@
+// src/admin/routes/auth.routes.ts
+
+import express, { Router } from 'express';
+import { AuthController } from '../../controllers/auth.controller.js';
+import { validateRegistrationInput } from '../../middleware/validate.middleware.js';
+
+const router: Router = express.Router();
+
+// Registration endpoint
+router.post('/register', validateRegistrationInput, AuthController.registerUser);
+// Login endpoint
+router.post('/login', AuthController.loginUser);
+// Logout endpoint
+router.post('/logout', AuthController.logoutUser);
+
+export default router;
