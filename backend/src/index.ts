3337dc58d47f479127c9dbc3e3bcf8e424057e15
MFH1118 <mishalalhaidar@gmail.com>
2025-02-09 04:41:27 +1100

    chore: remove unused components - Removed cart components - Removed product listings - Removed category components

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,19 +3,8 @@
 import express, { Express, Request, Response, Application} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import authRoutes from './routes/auth.routes.js'
-import productRoutes from './routes/product.routes.js';
-import categoryRoutes from './routes/category.routes.js';
-import wishListRoutes from './routes/wishlist.routes.js';
-import orderRoutes from './routes/order.routes.js';
-import adminRoutes from './admin/index.js';
-import prisma from './db/prisma.js';
-import { userCache } from './model/user.model.js';
-import { getSessionTypeInfo } from './utils/session-type.utils.js';
 import useragent from 'express-useragent';
 import cookieParser from 'cookie-parser';
-import addressRoutes from './routes/address.routes.js';
-import cartRoutes from './routes/cart.routes.js';
 
 dotenv.config();
 
@@ -27,14 +16,6 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/auth', authRoutes);
-app.use('/api/addresses', addressRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/wishlists', wishListRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/admin', adminRoutes);
 app.use(useragent.express());
 
 // Routes
@@ -48,23 +29,14 @@ app.get('/health', (req: Request, res: Response) => {
   });
 });
 
-app.get('/session-type', (req: Request, res: Response) => {
-  const sessionTypeInfo = getSessionTypeInfo(req);
-  res.status(200).json(sessionTypeInfo);
-});
-
 const cleanup = async () => {
   console.log('Cleaning up...');
-  userCache.close();
-  await prisma.$disconnect();
   console.log('Cleaned up successfully');
 }
 
 const run = async (): Promise<void> => {
   try {
     // Test the connection to the database
-    await prisma.$connect();
-    console.log('Connected to the database');
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
@@ -72,7 +44,6 @@ const run = async (): Promise<void> => {
     
   } catch (error: any) {
     console.error('Failed to start server:', error);
-    await prisma.$disconnect();
     process.exit(1);
   }
 };
