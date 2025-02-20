6cfee3389f89b677f784cb1b9340a4b6564c9b0a
MFH1118 <mishalalhaidar@gmail.com>
2025-02-21 06:32:41 +1100

    feat: remove role model and associated user role references from the database schema

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -3,7 +3,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { openAPI, phoneNumber } from 'better-auth/plugins';
-import { prisma } from '../db/prisma.js';
+import prisma  from '../db/prisma.js';
 import { signUpValidationHook } from '../hooks/signUp.validation.js';
 
 export const auth = betterAuth({
